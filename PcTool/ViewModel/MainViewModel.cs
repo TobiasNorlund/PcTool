c9b3fdb152cf5e17bf416c71648e64426a3da847
Tobias Norlund <tobias.norlund@tele2.se>
2013-04-23 21:54:02 +0200

    Småfix som stabiliserar

--- PcTool/ViewModel/MainViewModel.cs
+++ PcTool/ViewModel/MainViewModel.cs
@@ -86,7 +86,7 @@ namespace PcTool.ViewModel
             {
                 onHandshaked();
             }
-            else
+            else if(!IsConnected)
             {
                 onDisconnected();
             }
